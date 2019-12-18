0bc820834e390f582ecd91f7f43dd4dd06aea6a3
chenliwen123 <1410762621@qq.com>
2019-12-18 09:16:58 +0800

    新引入了 body-parser 和 swig 模块

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express=require('express');
-
+const swig=require('swig');
+const bodyparser=require('body-parser');
 const app=express();
 
+app.use(bodyparser.urlencoded({
+    extended:false,
+}))
+
+
 app.get('/',function (req,res) {
-    res.send('hello world');
+    res.render('index',{
+        name:"陈立文"
+    })
 })
 
-app.listen(3000,function () {
-    console.log('localhost:3000!!!!!!')
+app.post('/post',function (req,res) {
+    res.render('post',{
+        newname:req.body.name
+    })
+})
+
+app.set(express.static('./views'));
+app.engine('html',swig.renderFile);
+app.set('view engine','html')
+
+
+app.listen(3000,function (req,res) {
+   console.log('localhost:3000 ! ! ! !')
 })
